156bd885fd9bda50529508cbedc91ec4dda78835
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-04-04 23:03:53 +0900

    create_Match_App

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :chat_messages
+  has_many :chat_room_users
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  mount_uploader :img_name, ImgNameUploader
+  enum sex: { 男: 0, 女: 1 }
+  validates :img_name, presence: true
+end
